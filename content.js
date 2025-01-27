3b4fc5a5f4c57859a4cbc56265bc1d06a57f4cd9
Holin <holin.he@gmail.com>
2025-01-27 13:22:32 +0800

    First version

---
+++ content.js
@@ -0,0 +1,13 @@
+let lastKeyPressTime = 0;
+const doublePressThreshold = 300; // 双击时间间隔阈值（单位：毫秒）
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "b") {
+    const currentTime = new Date().getTime();
+    if (currentTime - lastKeyPressTime < doublePressThreshold) {
+      // 双击 b 键
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+    lastKeyPressTime = currentTime;
+  }
+});
